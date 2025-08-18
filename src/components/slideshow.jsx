6a5a42c1e847c324a1a93f3ae5649aeffd0bb827
theritikyadav11 <arvindkumar426390@gmail.com>
2025-08-19 00:54:31 +0530

    update slideshow

--- src/components/slideshow.jsx
+++ src/components/slideshow.jsx
@@ -3,31 +3,8 @@ import slide1 from "../assets/slideshow/slide1.png";
 import slide2 from "../assets/slideshow/slide2.png";
 import slide3 from "../assets/slideshow/slide3.png";
 import slide4 from "../assets/slideshow/slide4.png";
-import slide5 from "../assets/slideshow/slide5.png";
-import slide6 from "../assets/slideshow/slide6.png";
-import slide7 from "../assets/slideshow/slide7.png";
-import slide8 from "../assets/slideshow/slide8.png";
-import slide9 from "../assets/slideshow/slide9.png";
-import slide10 from "../assets/slideshow/slide10.png";
-import slide11 from "../assets/slideshow/slide11.png";
-import slide12 from "../assets/slideshow/slide12.png";
-import slide13 from "../assets/slideshow/slide13.png";
-import slide14 from "../assets/slideshow/slide14.png";
-import slide15 from "../assets/slideshow/slide15.png";
-import slide16 from "../assets/slideshow/slide16.png";
 
-const images = [
-  slide1,
-  slide2,
-  slide3,
-  slide4,
-  slide5,
-  slide6,
-  slide7,
-  slide8,
-  slide9,
-  slide10,
-];
+const images = [slide1, slide2, slide3, slide4];
 
 const Slideshow = () => {
   const [current, setCurrent] = useState(0);
@@ -37,7 +14,7 @@ const Slideshow = () => {
     if (timeoutRef.current) clearTimeout(timeoutRef.current);
     timeoutRef.current = setTimeout(
       () => setCurrent((prev) => (prev + 1) % images.length),
-      2000
+      4000
     );
     return () => clearTimeout(timeoutRef.current);
   }, [current]);
